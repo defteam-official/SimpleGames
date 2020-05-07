990a5a52271c98460b5a7e6c417391ebd513a5e8
allsaidX <allsaidx7@gmail.com>
2020-05-07 18:55:39 +0300

    update

---
+++ Simple_FlappyBird/Flappy_Bird/Assets/Scripts/ScriptMap.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    private class Block
+    {
+
+    }
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
